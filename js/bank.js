61a058f02e9543cc749c51f6315f9ce7623722c9
tushig <tushig11@gmail.com>
2019-07-02 22:40:51 -0500

    regex added

--- js/bank.js
+++ js/bank.js
@@ -4,6 +4,10 @@ window.onload = function() {
         var accountInfoList = [];
         var name = document.getElementById("acc");
         var deposit = document.getElementById("deposit");
+        var namePattern = document.getElementById("acc").pattern;
+        var depositPattern = document.getElementById("deposit").pattern;
+        var reName = new RegExp(namePattern);
+        var reDep = new RegExp(depositPattern);
 
         function printAccounts() {
             document.getElementById("data").innerHTML = " ";
@@ -15,13 +19,14 @@ window.onload = function() {
         return {
 
             add: function() {
-                if (name.value && deposit.value) {
+                console.log(reName.test(name.value));
+                if (reName.test(name.value) && reDep.test(deposit.value)) {
                     let newAcc = { name: name.value, balance: deposit.value }
                     accountInfoList.push(newAcc);
                     console.log(accountInfoList);
                     printAccounts();
                 } else {
-                    alert("Enter your Information ...");
+                    alert("Enter Correct information ...");
                 }
             }
         }
